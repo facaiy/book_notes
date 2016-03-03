9d6ab605ddc2c86754b34e14862812788e99158a
Yan Facai <yafc18@gmail.com>
2016-03-03 11:51:35 +0800

    完成算法IA第20章

---
+++ preconfig.py
@@ -0,0 +1,11 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.rcParams['axes.grid'] = False
+
+#import numpy as np
+#import pandas as pd
+#import itertools
+
+import logging
+logger = logging.getLogger()
