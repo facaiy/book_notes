53d22f31c874fbbb8765687f865f075ff214bdf8
Yan Facai (颜发才) <facai.yan@gmail.com>
2017-05-10 09:53:02 +0800

    FP: 完成第7章作业

---
+++ Manning_Functional_Programming_in_Scala/functional-programming/src/main/scala/io/github/facaiy/c7/NonBlocking.scala
@@ -0,0 +1,33 @@
+package io.github.facaiy.c7
+
+import java.util.concurrent.{Callable, CountDownLatch, ExecutorService}
+import java.util.concurrent.atomic.AtomicReference
+import language.implicitConversions
+
+/**
+ * Created by facai on 5/10/17.
+ */
+object NonBlocking {
+  sealed trait Future[A] {
+    private[c7] def apply(k: A => Unit): Unit
+  }
+
+  type Par[+A] = ExecutorService => Future[A]
+
+  // ex 7.10
+  def run[A](es: ExecutorService)(p: Par[A]): A = {
+    val ref = new AtomicReference[A]()
+    val latch = new CountDownLatch(1)
+
+    try {
+      p(es){ a => ref.set(a); latch.countDown() }
+      latch.await()
+    } catch {
+      case e: Exception =>
+        latch.countDown()
+        throw e
+    }
+
+    ref.get()
+  }
+}
